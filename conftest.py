7c884f7dfa07af699f29919cebb68f91d966b34e
Alexandr <102321532+h4rdhero@users.noreply.github.com>
2022-10-27 18:36:12 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def testing():
+    pytest.driver = webdriver.Chrome('C:\WebDriver\.chromedriver.exe')
+
+    pytest.driver.set_window_size(1400, 1000)
+
+    #Переходим на страницу авторизации
+    pytest.driver.get('http://petfriends.skillfactory.ru/login')
+
+    yield
+
+    pytest.driver.quit()
